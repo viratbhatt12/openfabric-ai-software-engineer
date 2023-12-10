65a049d21e99a7c4435b4c51d09222d47ca016a2
fti-vbhatt <vbhatt@ftilab.com>
2023-12-10 13:18:11 +0530

    Refactor main.py to load and use the pre-trained model

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import os
-import warnings
 from typing import Dict
-
+from model.model import NLPModel
 from openfabric_pysdk.utility import SchemaUtil
-
-from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
 
 from openfabric_pysdk.context import Ray, State
 from openfabric_pysdk.loader import ConfigClass
+
+from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
+
+# Load the Model
+model = NLPModel()
+model.load_model('model/model.h5')
 
 
 ############################################################
@@ -22,10 +24,17 @@ def config(configuration: Dict[str, ConfigClass], state: State):
 # Callback function called on each execution pass
 ############################################################
 def execute(request: SimpleText, ray: Ray, state: State) -> SimpleText:
+    """
+    Callback function called on each execution pass.
+
+    :param request: Input request.
+    :param ray: Openfabric execution ray.
+    :param state: Openfabric ray state.
+    :return: SimpleText response.
+    """
     output = []
     for text in request.text:
-        # TODO Add code here
-        response = 'Hello!'
+        response = model.predict(text)
         output.append(response)
 
     return SchemaUtil.create(SimpleText(), dict(text=output))
